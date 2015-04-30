f25723be792ad14dadba8ad8c17856e506dca016
Matt Davis <jiffyclub@gmail.com>
2015-04-30 16:33:58 -0700

    converting from simulation framework to orca

--- urbansim_defaults/datasources.py
+++ urbansim_defaults/datasources.py
@@ -1,79 +1,80 @@
-import numpy as np
-import pandas as pd
 import os
-import utils
 import uuid
-import yaml
-from urbansim.utils import misc
-import urbansim.sim.simulation as sim
-
 import warnings
+
+import numpy as np
+import orca
+import pandas as pd
+from urbansim.utils import misc
+import yaml
+
+import utils
 
 warnings.filterwarnings('ignore', category=pd.io.pytables.PerformanceWarning)
 pd.options.mode.chained_assignment = None
 
 
-@sim.injectable('settings', cache=True)
+@orca.injectable('settings', cache=True)
 def settings():
     with open(os.path.join(misc.configs_dir(), "settings.yaml")) as f:
         settings = yaml.load(f)
         # monkey patch on the settings object since it's pretty global
         # but will also be available as injectable
-        sim.settings = settings
+        orca.settings = settings
         return settings
 
 
-@sim.injectable('run_number', cache=True)
+@orca.injectable('run_number')
 def run_number():
     return misc.get_run_number()
 
 
-@sim.injectable('uuid', cache=True)
+@orca.injectable('uuid', cache=True)
 def uuid_hex():
     return uuid.uuid4().hex
 
 
-@sim.injectable('store', cache=True)
+@orca.injectable('store', cache=True)
 def hdfstore(settings):
     return pd.HDFStore(
         os.path.join(misc.data_dir(), settings["store"]),
         mode='r')
 
 
-@sim.injectable("summary", cache=True)
+@orca.injectable("summary", cache=True)
 def simulation_summary_data(run_number):
     return utils.SimulationSummaryData(run_number)
 
 
-@sim.injectable("building_type_map")
+@orca.injectable("building_type_map")
 def building_type_map(settings):
     return settings["building_type_map"]
 
 
-@sim.injectable("scenario")
+@orca.injectable("scenario")
 def scenario(settings):
     return settings["scenario"]
 
 
-@sim.injectable("scenario_inputs")
+@orca.injectable("scenario_inputs")
 def scenario_inputs(settings):
     return settings["scenario_inputs"]
 
 
-@sim.injectable("aggregations")
+@orca.injectable("aggregations")
 def aggregations(settings):
     if "aggregation_tables" not in settings or \
-    	settings["aggregation_tables"] is None:
-    	return []
-    return [sim.get_table(tbl) for tbl in settings["aggregation_tables"]]
+            settings["aggregation_tables"] is None:
+        return []
+    return [orca.get_table(tbl) for tbl in settings["aggregation_tables"]]
 
 
-@sim.injectable('building_sqft_per_job', cache=True)
+@orca.injectable('building_sqft_per_job', cache=True)
 def building_sqft_per_job(settings):
     return settings['building_sqft_per_job']
 
 
-@sim.table('buildings', cache=True)
+@orca.table('buildings', cache=True)
 def buildings(store, households, jobs, building_sqft_per_job, settings):
     df = store['buildings']
 
@@ -102,23 +103,22 @@ def buildings(store, households, jobs, building_sqft_per_job, settings):
     if fill_nas_cfg is not None:
         df = utils.table_reprocess(fill_nas_cfg, df)
 
-
     return df
 
 
-@sim.table('household_controls', cache=True)
+@orca.table('household_controls', cache=True)
 def household_controls():
     df = pd.read_csv(os.path.join(misc.data_dir(), "household_controls.csv"))
     return df.set_index('year')
 
 
-@sim.table('employment_controls', cache=True)
+@orca.table('employment_controls', cache=True)
 def employment_controls():
     df = pd.read_csv(os.path.join(misc.data_dir(), "employment_controls.csv"))
     return df.set_index('year')
 
 
-@sim.table('jobs', cache=True)
+@orca.table('jobs', cache=True)
 def jobs(store, settings):
     df = store['jobs']
 
@@ -133,7 +133,7 @@ def jobs(store, settings):
     return df
 
 
-@sim.table('households', cache=True)
+@orca.table('households', cache=True)
 def households(store, settings):
     df = store['households']
 
@@ -143,33 +143,33 @@ def households(store, settings):
 
     fill_nas_cfg = settings.get("table_reprocess", None)
     if fill_nas_cfg is not None:
-	    fill_nas_cfg = fill_nas_cfg.get("households", None)
+        fill_nas_cfg = fill_nas_cfg.get("households", None)
     if fill_nas_cfg is not None:
         df = utils.table_reprocess(fill_nas_cfg, df)
 
     return df
 
 
-@sim.table('parcels', cache=True)
+@orca.table('parcels', cache=True)
 def parcels(store):
     df = store['parcels']
     return df
 
 
 # these are shapes - "zones" in the bay area
-@sim.table('zones', cache=True)
+@orca.table('zones', cache=True)
 def zones(store):
     df = store['zones']
     return df
 
 
 # these are dummy returns that last until accessibility runs
-@sim.table("nodes", cache=True)
+@orca.table("nodes", cache=True)
 def nodes():
     return pd.DataFrame()
 
 
-@sim.table("logsums", cache=True)
+@orca.table("logsums", cache=True)
 def logsums(settings):
     logsums_index = settings.get("logsums_index_col", "taz")
     return pd.read_csv(os.path.join(misc.data_dir(),
@@ -178,11 +178,11 @@ def logsums(settings):
 
 
 # this specifies the relationships between tables
-sim.broadcast('nodes', 'buildings', cast_index=True, onto_on='node_id')
-sim.broadcast('nodes', 'parcels', cast_index=True, onto_on='node_id')
-sim.broadcast('logsums', 'buildings', cast_index=True, onto_on='zone_id')
-sim.broadcast('logsums', 'parcels', cast_index=True, onto_on='zone_id')
-sim.broadcast('parcels', 'buildings', cast_index=True, onto_on='parcel_id')
-sim.broadcast(
+orca.broadcast('nodes', 'buildings', cast_index=True, onto_on='node_id')
+orca.broadcast('nodes', 'parcels', cast_index=True, onto_on='node_id')
+orca.broadcast('logsums', 'buildings', cast_index=True, onto_on='zone_id')
+orca.broadcast('logsums', 'parcels', cast_index=True, onto_on='zone_id')
+orca.broadcast('parcels', 'buildings', cast_index=True, onto_on='parcel_id')
+orca.broadcast(
     'buildings', 'households', cast_index=True, onto_on='building_id')
-sim.broadcast('buildings', 'jobs', cast_index=True, onto_on='building_id')
+orca.broadcast('buildings', 'jobs', cast_index=True, onto_on='building_id')
--- urbansim_defaults/variables.py
+++ urbansim_defaults/variables.py
@@ -1,7 +1,8 @@
 import numpy as np
+import orca
 import pandas as pd
 from urbansim.utils import misc
-import urbansim.sim.simulation as sim
+
 import datasources
 import utils
 
@@ -11,49 +12,49 @@ import utils
 #####################
 
 
-@sim.column('buildings', 'node_id', cache=True)
+@orca.column('buildings', 'node_id', cache=True)
 def node_id(buildings, parcels):
     return misc.reindex(parcels.node_id, buildings.parcel_id)
 
 
-@sim.column('buildings', 'zone_id', cache=True)
+@orca.column('buildings', 'zone_id', cache=True)
 def zone_id(buildings, parcels):
     return misc.reindex(parcels.zone_id, buildings.parcel_id)
 
 
-@sim.column('buildings', 'general_type', cache=True)
+@orca.column('buildings', 'general_type', cache=True)
 def general_type(buildings, building_type_map):
     return buildings.building_type_id.map(building_type_map)
 
 
-@sim.column('buildings', 'sqft_per_unit', cache=True)
+@orca.column('buildings', 'sqft_per_unit', cache=True)
 def unit_sqft(buildings):
     return buildings.building_sqft / buildings.residential_units.replace(0, 1)
 
 
-@sim.column('buildings', 'lot_size_per_unit', cache=True)
+@orca.column('buildings', 'lot_size_per_unit', cache=True)
 def lot_size_per_unit(buildings, parcels):
     return misc.reindex(parcels.lot_size_per_unit, buildings.parcel_id)
 
 
-@sim.column('buildings', 'sqft_per_job', cache=True)
+@orca.column('buildings', 'sqft_per_job', cache=True)
 def sqft_per_job(buildings, building_sqft_per_job):
     return buildings.building_type_id.fillna(-1).map(building_sqft_per_job)
 
 
-@sim.column('buildings', 'job_spaces', cache=True)
+@orca.column('buildings', 'job_spaces', cache=True)
 def job_spaces(buildings):
     return (buildings.non_residential_sqft /
             buildings.sqft_per_job).fillna(0).astype('int')
 
 
-@sim.column('buildings', 'vacant_residential_units')
+@orca.column('buildings', 'vacant_residential_units')
 def vacant_residential_units(buildings, households):
     return buildings.residential_units.sub(
         households.building_id.value_counts(), fill_value=0)
 
 
-@sim.column('buildings', 'vacant_job_spaces')
+@orca.column('buildings', 'vacant_job_spaces')
 def vacant_job_spaces(buildings, jobs):
     return buildings.job_spaces.sub(
         jobs.building_id.value_counts(), fill_value=0)
@@ -64,7 +65,7 @@ def vacant_job_spaces(buildings, jobs):
 #####################
 
 
-@sim.column('households', 'income_quartile', cache=True)
+@orca.column('households', 'income_quartile', cache=True)
 def income_quartile(households):
     s = pd.Series(pd.qcut(households.income, 4, labels=False),
                   index=households.index)
@@ -72,12 +73,13 @@ def income_quartile(households):
     s = s.add(1)
     return s
 
-@sim.column('households', 'zone_id', cache=True)
+
+@orca.column('households', 'zone_id', cache=True)
 def zone_id(households, buildings):
     return misc.reindex(buildings.zone_id, households.building_id)
 
 
-@sim.column('households', 'node_id', cache=True)
+@orca.column('households', 'node_id', cache=True)
 def node_id(households, buildings):
     return misc.reindex(buildings.node_id, households.building_id)
 
@@ -87,12 +89,12 @@ def node_id(households, buildings):
 #####################
 
 
-@sim.column('jobs', 'node_id', cache=True)
+@orca.column('jobs', 'node_id', cache=True)
 def node_id(jobs, buildings):
     return misc.reindex(buildings.node_id, jobs.building_id)
 
 
-@sim.column('jobs', 'zone_id', cache=True)
+@orca.column('jobs', 'zone_id', cache=True)
 def zone_id(jobs, buildings):
     return misc.reindex(buildings.zone_id, jobs.building_id)
 
@@ -102,46 +104,46 @@ def zone_id(jobs, buildings):
 #####################
 
 
-@sim.column('parcels', 'parcel_size', cache=True)
+@orca.column('parcels', 'parcel_size', cache=True)
 def parcel_size(parcels, settings):
     return parcels.shape_area * settings.get('parcel_size_factor', 1)
 
 
-@sim.column('parcels', 'parcel_acres', cache=True)
+@orca.column('parcels', 'parcel_acres', cache=True)
 def parcel_acres(parcels):
     # parcel_size needs to be in sqft
     return parcels.parcel_size / 43560.0
 
-@sim.column('parcels', 'total_residential_units', cache=False)
+@orca.column('parcels', 'total_residential_units', cache=False)
 def total_residential_units(parcels, buildings):
     return buildings.residential_units.groupby(buildings.parcel_id).sum().\
         reindex(parcels.index).fillna(0)
 
 
-@sim.column('parcels', 'total_job_spaces', cache=False)
+@orca.column('parcels', 'total_job_spaces', cache=False)
 def total_job_spaces(parcels, buildings):
     return buildings.job_spaces.groupby(buildings.parcel_id).sum().\
         reindex(parcels.index).fillna(0)
 
 
-@sim.column('parcels', 'total_sqft', cache=True)
+@orca.column('parcels', 'total_sqft', cache=True)
 def total_sqft(parcels, buildings):
     return buildings.building_sqft.groupby(buildings.parcel_id).sum().\
         reindex(parcels.index).fillna(0)
 
 
-@sim.column('parcels', 'zoned_du', cache=True)
+@orca.column('parcels', 'zoned_du', cache=True)
 def zoned_du(parcels):
     return (parcels.max_dua * parcels.parcel_acres).\
         reindex(parcels.index).fillna(0).round().astype('int')
 
 
-@sim.column('parcels', 'zoned_du_underbuild')
+@orca.column('parcels', 'zoned_du_underbuild')
 def zoned_du_underbuild(parcels):
     return (parcels.zoned_du - parcels.total_residential_units).clip(lower=0)
 
 
-@sim.column('parcels', 'ave_sqft_per_unit')
+@orca.column('parcels', 'ave_sqft_per_unit')
 def ave_sqft_per_unit(parcels, nodes, settings):
     if len(nodes) == 0:
         # if nodes isn't generated yet
@@ -154,19 +156,19 @@ def ave_sqft_per_unit(parcels, nodes, settings):
 
 
 # this just changes the column name for reverse compatibility
-@sim.column('parcels', 'ave_unit_size')
+@orca.column('parcels', 'ave_unit_size')
 def ave_unit_size(parcels):
     return parcels.ave_sqft_per_unit
 
 
-@sim.column('parcels', 'lot_size_per_unit')
+@orca.column('parcels', 'lot_size_per_unit')
 def log_size_per_unit(parcels):
     return parcels.parcel_size / parcels.total_residential_units.replace(0, 1)
 
 
 # returns the oldest building on the land and fills missing values with 9999 -
 # for use with historical preservation
-@sim.column('parcels', 'oldest_building')
+@orca.column('parcels', 'oldest_building')
 def oldest_building(parcels, buildings):
     return buildings.year_built.groupby(buildings.parcel_id).min().\
         reindex(parcels.index).fillna(9999)
