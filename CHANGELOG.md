200083d86a1d28f8c37b9e8ba27eb0bda0147477
Sam Maurer <maurer@berkeley.edu>
2019-07-10 10:46:15 -0700

    Updating version number

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # UrbanSim Defaults change log
 
+#### 0.1.1 (2019-07-10)
+
+- adds optional parameter to `utils.lcm_simulate()` for `alternative_ratio`
+
 #### 0.1 (2019-06-19)
 
 - initial release
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='urbansim_defaults',
-    version='0.1',
+    version='0.1.1',
     description='Installs and runs the urbansim defaults.',
     author='UrbanSim, Inc.',
     author_email='info@urbansim.com',
